36ad54645ee15bba7a42fd3c1dc33ec4b8eafe66
GBTC441004UR <gfeliz3@montgomerycollege.edu>
2018-03-16 10:00:03 -0400

    Update User.java

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -17,10 +17,10 @@ public class User {
     private String password;
 
     @Column(name = "first_name")
-    private String first_name;
+    private String firstName;
 
     @Column(name = "last_name")
-    private String last_name;
+    private String lastName;
 
     @Column(name = "enabled")
     private boolean enabled;
@@ -57,20 +57,20 @@ public class User {
         this.password = password;
     }
 
-    public String getFirst_name() {
-        return first_name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public String getLast_name() {
-        return last_name;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public boolean isEnabled() {
